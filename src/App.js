2a04e0cfce18e6df6a155e85219b5412f54592e3
nick.durbin <nick.durbin@gmail.com>
2019-09-13 19:06:38 -0400

    Initial commit and install of components.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-import TabNav from "./components/TabNav.js";
-import Header from "./components/Header.js";
-
+import TabNav from "./components/TabNav";
+import Header from "./components/Header";
+import WelcomePage from './components/WelcomePage';
+import CharacterList from './components/CharacterList';
+import LocationList from './components/LocationsList';
 
 export default function App() {
   return (
     <main>
       <Header />
       {/* <TabNav /> */}
+      <WelcomePage />
     </main>
   );
 }
