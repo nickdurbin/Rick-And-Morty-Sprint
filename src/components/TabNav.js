2f9fa25813712f1f7ca7baae49bc3e746f05c939
nick.durbin <nick.durbin@gmail.com>
2019-09-13 20:04:18 -0400

    Setup the navlinks for the navbar.

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Input, Menu, Segment } from 'semantic-ui-react'
+import { Input, Menu } from 'semantic-ui-react'
 import { NavLink } from "react-router-dom";
 
 export default class TabNav extends Component {
@@ -13,26 +13,34 @@ export default class TabNav extends Component {
     return (
       <div>
         <Menu pointing>
+          <NavLink to='/'>
           <Menu.Item
             name='home'
             active={activeItem === 'home'}
             onClick={this.handleItemClick}
           />
+          </NavLink>
+          <NavLink to='/characters'>
           <Menu.Item
             name='characters'
             active={activeItem === 'characters'}
             onClick={this.handleItemClick}
           />
+          </NavLink>
+          <NavLink to='/location'>
           <Menu.Item
             name='location'
             active={activeItem === 'location'}
             onClick={this.handleItemClick}
           />
+          </NavLink>
+          <NavLink to='/episodes'>
             <Menu.Item
             name='episodes'
             active={activeItem === 'episodes'}
             onClick={this.handleItemClick}
           />
+          </NavLink>
           <Menu.Menu position='right'>
             <Menu.Item>
               <Input icon='search' placeholder='Search...' />
